d113952604391681036b12cbe8c43326993dadce
castaneai <castaneai@castaneai.net>
2018-05-16 02:42:13 +0900

    最低限サービスが動くように

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,21 @@
 import * as functions from 'firebase-functions';
+const speech = require('@google-cloud/speech');
 const formidable = require('formidable');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send(`
-    <form action="upload" enctype="multipart/form-data" method="post">
-    <input type="file" name="upload">
-    <input type="submit" value="upload">
-    </form>`)
+const MAX_DURATION = 60;
+
+export const index = functions.https.onRequest((req, res) => {
+    res.send(`
+<h1>インスト判定くん</h1>
+<p>楽曲をアップロードすると、インスト音源かどうかを判定してくれます</p>
+<form action="upload" enctype="multipart/form-data" method="post">
+    <p><input type="file" name="upload"></p>
+    <p><label>判定する秒数（最大60秒）：<input type="text" name="duration" value="10"></label></p>
+    <button type="submit">判定する</button>
+</form>`)
 });
 
 export const upload = functions.https.onRequest((req, res: functions.Response) => {
@@ -17,22 +23,46 @@ export const upload = functions.https.onRequest((req, res: functions.Response) =
     form.parse(req, (err, fields, files) => {
         const file = files['upload'];
         if (!file) {
-            res.send('upload file not found');
-            return;
+            res.end('upload file not found');
         }
-        console.info(`upload file type: ${file.type}, path: ${file.path}`);
+        const duration = Math.min(parseInt(fields['duration']), MAX_DURATION);
+        const client = new speech.SpeechClient();
+
+        const request = {
+            config: {
+                encoding: 'LINEAR16',
+                sampleRateHertz: 44100,
+                languageCode: 'ja-JP',
+            },
+            interimResults: false,
+        };
+
+        var isInst = false;
+        const recognizeStream = client
+            .streamingRecognize(request)
+            .on('error', console.error)
+            .on('data', data => {
+                isInst = true;
+                const ts = data.results[0].alternatives[0].transcript;
+                console.log(
+                    `Transcription: ${ts}`
+                );
+                res.send(`<h1>インスト曲ではありません！（ボーカル入りです）</h1>
+                <h2>抽出された歌詞</h2>
+                <pre>${ts}</pre>
+                <a href="/">←戻る</a>`)
+            })
+            .on('end', () => {
+                if (!isInst) {
+                    res.send(`<h1>インスト曲です！</h1><a href="/">←戻る</a>`)
+                }
+            })
+
         ffmpeg(file.path)
             .noVideo()
-            .format('wav')
-            .on('error', (err, stdout, stderr) => {
-                console.error(err.message);
-                console.error(stdout);
-                console.error(stderr);
-            })
-            .on('end', () => console.info('ended!'))
-            .on('progress', (prog) => {
-                console.log(`progress: ${prog.percent}%`);
-            })
-            .pipe(res, { end: true });
+            .duration(duration)
+            .format('s16le')
+            .audioChannels(1)
+            .pipe(recognizeStream, { end: true });
     });
 });
